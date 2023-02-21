7440a66067f7130ee21e5c5d98cfea80d9de89f8
Abdullah Ayoola <100965547+abdullah43577@users.noreply.github.com>
2023-02-22 00:05:07 +0100

    restructured my code from scratch

--- app/script.js
+++ app/script.js
@@ -7,6 +7,7 @@ console.log(data);
 class App {
   constructor() {
     this.main = document.querySelector("main");
+    this.userComment;
 
     this.main.addEventListener("click", this.domTraversePlus.bind(this));
     this.main.addEventListener("click", this.domTraverseMinus.bind(this));
@@ -15,6 +16,7 @@ class App {
     this.main.addEventListener("click", this.domTraverseEdit.bind(this));
     this.main.addEventListener("click", this.domTraverseComment.bind(this));
     this.main.addEventListener("click", this.domTraverseReplyBox.bind(this));
+    this.main.addEventListener("click", this.addComment.bind(this));
     this.renderDomEl();
   }
 
@@ -264,11 +266,100 @@ class App {
             placeholder="Add a comment..."
           ></textarea>
 
-          <button>reply</button>
+          <button class="send">SEND</button>
         </div>
       </section>`;
 
     this.main.insertAdjacentHTML("beforeend", html);
+  }
+
+  addComment(e) {
+    let sendBtn = e.target.closest(".send");
+    if (!sendBtn) return;
+    let comment = sendBtn.previousElementSibling;
+    this.userComment = comment.value;
+    // this.main.insertAdjacentHTML(
+    //   "beforeend",
+    //   this.renderComment(this.userComment)
+    // );
+    // create a range and insert the HTML string as a fragment
+    let range = document.createRange();
+    let fragment = range.createContextualFragment(
+      this.renderComment(this.userComment)
+    );
+
+    let lastChild = this.main.lastElementChild;
+
+    this.main.insertBefore(fragment, lastChild);
+    comment.value = "";
+  }
+
+  renderComment(value) {
+    // prettier-ignore
+    return `   <div class="container">
+              <div class="vote--container">
+                <figure>
+                  <img src="./images/icon-plus.svg" alt="icon-plus" />
+                </figure>
+
+                <span>12</span>
+
+                <figure>
+                  <img src="./images/icon-minus.svg" alt="icon-minus" />
+                </figure>
+
+                <!-- mobile reply btn section -->
+                <div class="deledit">
+                  <div class="delete">
+                    <figure>
+                      <img src="./images/icon-delete.svg" alt="icon-delete" />
+                    </figure>
+                    <p>Delete</p>
+                  </div>
+
+                  <div class="edit">
+                    <figure>
+                      <img src="./images/icon-edit.svg" alt="icon-edit" />
+                    </figure>
+                    <p>Reply</p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="content--container">
+                <div class="details">
+                  <div class="image--section">
+                    <img
+                      src="${data.currentUser.image.png}"
+                      alt="${data.currentUser.username}"
+                    />
+                    <p class="name">${data.currentUser.username}</p>
+                    <p class="you">you</p>
+                    <p>${data.comments[1].replies[1].createdAt}</p>
+                  </div>
+                  <div class="deledit">
+                    <div class="delete">
+                      <figure>
+                        <img src="./images/icon-delete.svg" alt="icon-delete" />
+                      </figure>
+                      <p>Delete</p>
+                    </div>
+
+                    <div class="edit">
+                      <figure>
+                        <img src="./images/icon-edit.svg" alt="icon-edit" />
+                      </figure>
+                      <p>Edit</p>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="content">
+                <span>@johanssbro</span>
+                   ${value}
+                </div>
+              </div>
+            </div>`;
   }
 
   domTraversePlus(e) {
@@ -288,7 +379,6 @@ class App {
     if (!replybtn) return;
     console.log(replybtn);
     let parent = replybtn.parentElement.parentElement.parentElement;
-    console.log(parent);
 
     parent.insertAdjacentHTML("afterend", this.replyBox());
   }
